6446b2b4885e0387f4ff23bb2d07eb5efc3f3e4a
page1762 <page1762@gmail.com>
2023-07-30 12:31:50 +0900

    답변을 가져올 때 질문도 같이 가져오게

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -75,4 +75,12 @@ public class AnswerRepositoryTests {
     assertThat(a.getContent()).isEqualTo("sbb는 질문, 답변 게시판입니다.");
   }
 
+  @Test
+  void 관련된_question_조회() {
+    Answer a = answerRepository.findById(1).get();
+    Question q = a.getQuestion();
+
+    assertThat(a.getId()).isEqualTo(1);
+  }
+
 }
