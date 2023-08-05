4f383ee44a3b21922358b855fd06f00cb06a2490
page1762 <page1762@gmail.com>
2023-08-05 14:43:02 +0900

    답변등록 폼 구현

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -4,20 +4,40 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@RequestMapping("/question")
 @Controller
-@RequiredArgsConstructor
+@RequiredArgsConstructor // 생성자 주입
+// 컨트롤러는 Repository가 있는지 몰라야 한다.
+// 서비스는 브라우저라는 것이 이 세상에 존재하는지 몰라야 한다.
+// 리포지터리는 서비스가 이 세상이 있는지 몰라야 한다.
+// 서비스는 컨트롤러를 몰라야 한다.
+// DB는 리포지터리를 몰라야 한다.
+// SPRING DATA JPA는 MySQL을 몰라야 한다.
+// SPRING DATA JPA(리포지터리) -> JPA -> 하이버네이트 -> JDBC -> MySQL Driver -> MySQL
 public class QuestionController {
+  // @Autowired 필드 주입
+  private final QuestionService questionService;
 
-private final QuestionService questionService;
-
-  @GetMapping("/question/list")
+  @GetMapping("/list")
+  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일을 뷰로 삼는다.
   public String list(Model model) {
     List<Question> questionList = questionService.getList();
-    model.addAttribute("questionList",questionList);
+    // 미리 실행된 question_list.html에서
+    // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
+    model.addAttribute("questionList", questionList);
     return "question_list";
+  }
+
+  @GetMapping(value = "/detail/{id}")
+  public String detail(Model model, @PathVariable int id) {
+    Question question = questionService.getQuestion(id);
+
+    model.addAttribute("question", question);
+    return "question_detail";
   }
 }
--- src/main/java/com/exam/sbb/question/QuestionService.java
+++ src/main/java/com/exam/sbb/question/QuestionService.java
@@ -1,20 +1,33 @@
 package com.exam.sbb.question;
 
+import com.exam.sbb.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
+
   private final QuestionRepository questionRepository;
 
   @Autowired
-  public QuestionService(QuestionRepository questionRepository){
+  public QuestionService(QuestionRepository questionRepository) {
     this.questionRepository = questionRepository;
   }
 
   public List<Question> getList() {
-    return this.questionRepository.findAll();
+    return questionRepository.findAll();
+  }
+
+  public Question getQuestion(int id) throws DataNotFoundException {
+    Optional<Question> oq = questionRepository.findById(id);
+
+    if (oq.isPresent()) {
+      return oq.get();
+    }
+
+    throw new DataNotFoundException("question not found");
   }
 }
